cbc48b4e161270c9a5a15c0f353fbd32794bec7b
javierenriquegigena-upc <javierenriquegigena@upc.edu.ar>
2025-08-31 10:30:51 -0300

    Error en código reutilizado
    InvalidOperationException: You are trying to read Input using the UnityEngine.Input class, but you have switched active Input handling to Input System package in Player Settings.
    UnityEngine.Internal.InputUnsafeUtility.GetAxisRaw (System.String axisName) (at <921937907c924850a1d64935957a9b52>:0)
    UnityEngine.Input.GetAxisRaw (System.String axisName) (at <921937907c924850a1d64935957a9b52>:0)
    playerMovment.Update () (at Assets/script/playerMovment.cs:23)

--- Assets/script/playerMovment.cs
+++ Assets/script/playerMovment.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerMovment : MonoBehaviour
 {
+    
+    public float speed;
+    private Rigidbody2D _rigidbody2D;
+    public float _horizontalDirection;
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+  
     void Start()
     {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        _horizontalDirection = Input.GetAxisRaw("Horizontal");
+
+        transform.position = new Vector2(transform.position.x + _horizontalDirection * speed * Time.deltaTime, transform.position.y);
+        //_rigidbody2D.linearVelocityX = _horizontalDirection * speed;
+
         
+
+
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
     }
+
 }
