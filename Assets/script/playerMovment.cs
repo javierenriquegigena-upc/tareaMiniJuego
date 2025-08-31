e22bee1fef88a144c285a1966867cd8197690af9
javierenriquegigena-upc <javierenriquegigena@upc.edu.ar>
2025-08-31 10:49:11 -0300

    Error playerMovment
    Sigue el error, este código funciona en la versión 6000.0.45f1

--- Assets/script/playerMovment.cs
+++ Assets/script/playerMovment.cs
@@ -1,20 +1,18 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class playerMovment : MonoBehaviour
 {
-    
+
     public float speed;
     private Rigidbody2D _rigidbody2D;
     public float _horizontalDirection;
-   
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
-  
+
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        
+
     }
 
     // Update is called once per frame
@@ -22,17 +20,8 @@ public class playerMovment : MonoBehaviour
     {
         _horizontalDirection = Input.GetAxisRaw("Horizontal");
 
-        transform.position = new Vector2(transform.position.x + _horizontalDirection * speed * Time.deltaTime, transform.position.y);
-        //_rigidbody2D.linearVelocityX = _horizontalDirection * speed;
-
-        
-
-
-        
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene("SampleScene");
-        }
+        //transform.position = new Vector2(transform.position.x + horizontalDirection * speed * Time.deltaTime, transform.position.y);
+        _rigidbody2D.linearVelocityX = _horizontalDirection * speed;
     }
 
 }
